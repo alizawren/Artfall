3f51671c258b78959517d5df125780c07b465c2e
alizawren <alissaren98@gmail.com>
2018-12-26 19:19:13 -0800

    Chat app example

---
+++ chatapp/server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var http = require('http');
+var app = express();
+var httpServer = http.createServer(app);
+
+var io = require('socket.io')(httpServer);
+
+app.use(express.static('src'));
+
+httpServer.listen(3000, function() {
+    console.log("Listening on port 3000");
+});
+
+var numClients = 0;
+io.on('connection', function(client) {
+    console.log('Client', numClients++, 'connected.');
+
+    client.on('join', function(data) {
+        console.log(data);
+    });
+
+    client.on("chat", function(msg) {
+        console.log(msg);
+        client.broadcast.emit('chat msg', msg);
+    })
+});
