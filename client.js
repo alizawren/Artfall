0d878042431f37702f0f7a17093989038c7f2005
alizawren <alissaren98@gmail.com>
2018-12-27 12:48:42 -0800

    Fixed chat, can change username

--- client.js
+++ client.js
@@ -1,35 +1,76 @@
-var inputElem = null;// = document.querySelector('.chatMessage');
+var chatInput = null;// = document.querySelector('.chatMessage');
 var messages = null;// = document.querySelector('.messages');
+var changeUsername = null;
+var changeUsernameOverlay = null;
 
-$( document ).ready(function() {
-  inputElem = document.getElementById('chatMessage');
-  messages = document.getElementById('messages')
-  console.log(inputElem);
-});
-const socket = io('http://localhost:8000');
-
-function createHTMLMessage(msg,source) {
-    console.log(msg);
+function createHTMLMessage(msg, source) {
     var li = document.createElement("li");
     var div = document.createElement("div");
-    div.innerHTML += msg;
-    div.className += "messageInstance " + source;
+    if (source == 'info') {
+        div.innerHTML = msg;
+        div.className += "messageInstance " + source;
+    }
+    else if (source == 'client') {
+        div.innerHTML = "You: " + msg;
+        div.className += "messageInstance " + source;
+    }
+    else {
+        div.innerHTML = source + ": " + msg;
+        div.className += "messageInstance " + 'server';
+    }
     li.appendChild(div);
     messages.appendChild(li);
 }
 
-inputElem.addEventListener('keypress', function(e) {
-    var key = e.which || e.keyCode;
-    if (key === 13) {
-        createHTMLMessage(inputElem.value, 'client');
-        socket.emit('chat', inputElem.value);
+$(document).ready(function () {
+    chatInput = document.getElementById('chatMessage');
+    messages = document.getElementById('messages')
+
+    chatInput.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+        if (key === 13) {
+            createHTMLMessage(chatInput.value, 'client'); // Create a message from the client
+            socket.emit('chat', chatInput.value, 'client'); // send message to server
+            chatInput.value = '';
+        }
+    });
+
+    changeUsernameButton = document.getElementById('change-username-button');
+    changeUsernameOverlay = document.getElementById('change-username-overlay');
+    changeUsernameInput = document.getElementById('change-username-input');
+    changeUsernameButton.onclick = () => {
+        changeUsernameOverlay.style.display = 'block';
     }
+    document.getElementById('change-username-overlay-close').onclick = () => {
+        changeUsernameOverlay.style.display = 'none';
+    }
+    changeUsernameInput.addEventListener('keypress', function (e) {
+        var key = e.which || e.keyCode;
+        if (key === 13) {
+            let name = 'Sammy';
+            createHTMLMessage(`${name} has changed their name to ${changeUsernameInput.value}.`, 'info'); // Create a message for client side
+            socket.emit('chat', `${name} has changed their name to ${changeUsernameInput.value}.`, 'info'); // Create a message for server
+            socket.emit('change username', {username: changeUsernameInput.value}); // change username
+            changeUsernameOverlay.style.display = 'none';
+        }
+    });
 });
+const socket = io('http://localhost:3000');
 
-socket.on('connect', function(data) {
+socket.on('connect', function () {
     socket.emit('join', 'Hello server from client');
+})
+
+socket.on('connect msg', function () {
+    let name = 'Sammy';
+    createHTMLMessage(`${name} has entered the chatroom.`, 'info');
 });
 
-socket.on('chat msg', function(msg) {
-    createHTMLMessage(msg, 'server');
+socket.on('chat msg', function (msg, source) {
+    createHTMLMessage(msg, source); // Create a message from the server
 });
+
+socket.on('disconnect msg', function () {
+    let name = 'Sammy';
+    createHTMLMessage(`${name} has left the chatroom.`, 'info');
+})
--- server.js
+++ server.js
@@ -1,80 +1,55 @@
-//!/usr/bin/env node
-var WebSocketServer = require('websocket').server;
+var express = require('express');
 var http = require('http');
+var app = express();
+var httpServer = http.createServer(app);
 
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-    response.writeHead(404);
-    response.end();
-});
-server.listen(8080, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
+var io = require('socket.io')(httpServer);
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
+app.use(express.static('src'));
+
+httpServer.listen(3000, function() {
+    console.log("Listening on port 3000");
 });
 
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-var numClients = 0;
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
+var chatHistory = {};
 
-    var connection = request.accept('echo-protocol', request.origin);
-    numClients++;
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('chat', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
+var numclientSockets = 0;
+io.on('connection', function(clientSocket) {
+    console.log('clientSocket', numclientSockets++, 'connected.');
+    clientSocket.username = 'Anonymous';
+
+    clientSocket.on('change username', (data) => {
+        clientSocket.username = data.username;
+    }) 
+    clientSocket.broadcast.emit('connect msg');
+
+    // clientSocket.on('join', function(data) {
+    //     console.log(data);
+    //     console.log('join');
+    // });
+
+    
+    // clientSocket.on('leave', function() {
+    //     console.log('left')
+    // })
+
+    clientSocket.on('connect', function() {
+        clientSocket.broadcast.emit('connect msg');
+    })
+
+    clientSocket.on('disconnect', function () {
+        numclientSockets--;
+        clientSocket.broadcast.emit('disconnect msg');
+    })
+
+    clientSocket.on("chat", function(msg, source) { // When receiving a message from a client
+        chatHistory[msg] = source;
+        if (source == 'client') {
+            clientSocket.broadcast.emit('chat msg', msg, clientSocket.username); // Send message to all clients
         }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
+        if (source == 'info') {
+            clientSocket.broadcast.emit('chat msg', msg, 'info');
         }
-    });
-    connection.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-    });
+        
+    })
 });
-//
-// var express = require('express');
-// var http = require('http');
-// var app = express();
-// var httpServer = http.createServer(app);
-//
-// var io = require('socket.io')(httpServer);
-//
-// app.use(express.static('src'));
-//
-// httpServer.listen(3000, function() {
-//     console.log("Listening on port 3000");
-// });
-//
-// var numClients = 0;
-// io.on('connection', function(client) {
-//     console.log('Client', numClients++, 'connected.');
-//
-//     client.on('join', function(data) {
-//         console.log(data);
-//     });
-//
-//     client.on("chat", function(msg) {
-//         console.log(msg);
-//         client.broadcast.emit('chat msg', msg);
-//     })
-// });
